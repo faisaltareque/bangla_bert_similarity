c406eadd5f47e83fcc8be59729b44f60b63c6f9c
Faisal Tareque <faisaltareque98@gmail.com>
2022-06-16 17:42:46 +0600

    New test

--- tests/test_bangla_bert_similarity.py
+++ tests/test_bangla_bert_similarity.py
@@ -1,7 +1,7 @@
 from bangla_bert_similarity import similarity
 
 def test_similarity():
-    sentence1 = 'যোগাযোগ মন্ত্রী বলেছেন এ নিয়ে আমরা আলোচনা করেছি'
-    sentence2 = 'মানিকগঞ্জের দৌলতপুর উপজেলা আওয়ামী লীগের ত্রিবার্ষিক সম্মেলন অনুষ্ঠিত হয়েছে'
+    sentence1 = 'তোমার সাথে দেখা হয়ে ভালো লাগলো।'
+    sentence2 = 'আপনার সাথে দেখা হয়ে ভালো লাগলো।'
     score = similarity.score(sentence1, sentence2)
-    assert str(score) == '[0.38779554]'
+    assert str(score) == '[0.83910286]'
